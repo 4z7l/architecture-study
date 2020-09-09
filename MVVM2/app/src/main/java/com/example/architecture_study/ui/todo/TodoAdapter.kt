5f6dad9224fd3997a1666a244da456525d1e1131
4z7l <7d4z7l@naver.com>
2020-09-09 19:55:07 +0900

    Feat : delete todo

--- MVVM2/app/src/main/java/com/example/architecture_study/ui/todo/TodoAdapter.kt
+++ MVVM2/app/src/main/java/com/example/architecture_study/ui/todo/TodoAdapter.kt
@@ -6,31 +6,52 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.architecture_study.R
 import com.example.architecture_study.data.Todo
-import kotlinx.android.synthetic.main.item_todo.*
 import kotlinx.android.synthetic.main.item_todo.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
 
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
     private var mTodoItems : List<Todo> = listOf()
+    private var mitemClickListener: OnItemClickListener?=null
+
+    fun setItemClickListener(itemClickListener: OnItemClickListener) {
+        this.mitemClickListener = itemClickListener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent,false))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent,false)
+
+        return ViewHolder(view, mitemClickListener)
     }
 
     override fun getItemCount(): Int {
         return mTodoItems.size
     }
 
+    fun getItem(position: Int): Todo {
+        return mTodoItems.get(position)
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(mTodoItems[position])
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    class ViewHolder(itemView: View, itemClickListener: OnItemClickListener?) : RecyclerView.ViewHolder(itemView) {
         private val title = itemView.item_todo_title
         private val description = itemView.item_todo_description
         private val date = itemView.item_todo_date
+
+        init {
+            itemView.setOnClickListener{
+                itemClickListener?.onItemClick(adapterPosition)
+            }
+        }
 
         fun bind(item:Todo){
             title.text = item.title
